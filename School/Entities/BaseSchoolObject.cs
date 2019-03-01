2aac63ec917189fc9ef5590e7586550abd122876
Abdel Mejia <bhalut.bear@gmail.com>
2019-03-01 14:52:24 -0500

    Add BaseSchoolObject class abstract

---
+++ School/Entities/BaseSchoolObject.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CoreSchool.Entities
+{
+    public abstract class BaseSchoolObject
+    {
+        public string UniqueId { get; private set; }
+        public string Name { get; set; }
+
+        public BaseSchoolObject() => UniqueId = Guid.NewGuid().ToString();
+    }
+}
