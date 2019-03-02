ed2f4464c6edff242eefbddad3e57833999a46db
Abdel Mejia <bhalut.bear@gmail.com>
2019-03-02 15:26:28 -0500

    Add KeyDictionary Enum

---
+++ School/Entities/KeyDictionary.cs
@@ -0,0 +1,12 @@
+using System.Data.Common;
+namespace CoreSchool.Entities
+{
+    public enum KeyDictionary
+    {
+        School,
+        Studient,
+        Asignature,
+        Test,
+        Course
+    }
+}
